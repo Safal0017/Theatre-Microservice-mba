2118bb266a1c6b9f6b5bd6c4b860e2bae9e875ad
safal patel <safalpatelsjp203039@gmail.com>
2021-08-21 05:59:02 +0530

    Initializing Microservice-3 (theatre-microservice) of mba

---
+++ src/main/java/com/upgrad/theatremicroservice/TheatreMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.upgrad.theatremicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreMicroserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TheatreMicroserviceApplication.class, args);
+		System.out.println("Theatre Microservice Application started and running up on the server!!");
+	}
+
+}
---
+++ src/test/java/com/upgrad/theatremicroservice/TheatreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.theatremicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
